6ffff8ac92c283eb6da936ff77ccc431e635a788
Alexander Misyura <87127837+AlexanderMisyura@users.noreply.github.com>
2024-01-24 20:00:23 +0300

    feat: add findAllOccurrences task

--- src/arrays-tasks.js
+++ src/arrays-tasks.js
@@ -77,8 +77,14 @@ function findElement(arr, value) {
  *    findAllOccurrences([ null, undefined, null ], null) => 2
  *    findAllOccurrences([ true, 0, 1, 'true' ], true) => 1
  */
-function findAllOccurrences(/* arr, item */) {
-  throw new Error('Not implemented');
+function findAllOccurrences(arr, item) {
+  return arr.reduce((acc, curr) => {
+    let counter = acc;
+    if (curr === item) {
+      counter += 1;
+    }
+    return counter;
+  }, 0);
 }
 
 /**
